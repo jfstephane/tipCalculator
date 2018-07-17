84567037d8876e43084618be6819dae4b92fe8d8
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2018-07-17 12:51:38 -0400

    first
    first

---
+++ tiptip/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  tiptip
+//
+//  Created by Jules Frantz Stephane Loubeau on 7/17/18.
+//  Copyright © 2018 Jules Frantz Stephane Loubeau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+  
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        //print ("hello")
+        
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        totalLabel.text = String(format:"$%.2f",tip)
+        tipLabel.text = String(format:"$%.2f",total)
+    }
+    
+    
+    
+}
+
